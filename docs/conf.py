ecd520759e942992ddef93d28c799c747cc2f55a
Elchinchel <Elchin751@gmail.com>
2024-08-06 00:41:12 +0300

    Update project configuration
    Changes:
    -- Exclude tests from build
    -- Add config for ruff's isort
    -- Add extra for docs building

--- docs/conf.py
+++ docs/conf.py
@@ -5,23 +5,36 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import datetime
+
+
+year = datetime.datetime.now().year
+
 
 project = 'Communica'
-copyright = '2023, Elchin Sarkarov'
+copyright = f'{year}, P.I.V.O.'
 author = 'Elchinchel'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'myst_parser',
+    'autodoc2',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
+autodoc2_packages = [
+    "../communica",
+]
+autodoc2_hidden_objects = ['private', 'inherited']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_logo = '_static/servershake.svg'
+html_theme = 'python_docs_theme'
 html_static_path = ['_static']
