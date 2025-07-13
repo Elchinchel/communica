8767eb76a4559609182a3e980da76575a7bdf294
Elchinchel <elchin751@gmail.com>
2025-07-13 12:28:48 +0300

    Add nox for testing on multiple python versions

---
+++ noxfile.py
@@ -0,0 +1,25 @@
+import nox
+
+
+@nox.session
+def docs(session: nox.Session):
+    session.install('sphinx', 'myst-parser', 'python-docs-theme', 'sphinx-autodoc2')
+    session.run('python3', '-m', 'sphinx', *session.posargs)
+
+
+@nox.session(
+    python=[
+        'python3.8',
+        'python3.9',
+        'python3.10',
+        'python3.11',
+        'python3.12',
+        'python3.13',
+    ]
+)
+def test(session: nox.Session):
+    session.install(
+        '.[orjson, adaptix, rabbitmq]',
+        'pytest', 'pytest-cov', 'pytest-asyncio'
+    )
+    session.run('pytest', *session.posargs)
